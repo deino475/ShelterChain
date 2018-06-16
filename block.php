4dd207819ad1cfbe44f7a18d9a3fe19f9e39a8bb
deino475 <niledixon475@gmail.com>
2018-06-16 17:06:53 -0500

    Improved the block class, built UI for mining block;

--- block.php
+++ block.php
@@ -4,20 +4,21 @@ class Block {
 	public $data;
 	public $previous_hash;
 	public $hash;
+	public $nonce;
 
-	public function __construct($data, $previous_hash) {
+	public function __construct($data, $previous_hash, $time_stamp) {
 		$this->data = $data;
-		$this->time_stamp = time();
+		$this->time_stamp = $time_stamp;
 		$this->previous_hash = $previous_hash;
 		$this->hash = $this->hash_data();
+		$this->nonce = 0;
 	}
 
 	public function hash_data() {
-		$nonce = 0;
-		while (!is_numeric(substr(sha1($nonce . json_encode($this->data) . $this->time_stamp . $this->previous_hash),0,1) )) {
-			$nonce++;
+		while (!is_numeric(substr(sha1($this->nonce . json_encode($this->data) . $this->time_stamp . $this->previous_hash),0,1) )) {
+			$this->nonce++;
 		}
-		return sha1($nonce . json_encode($this->data) . $this->time_stamp . $this->previous_hash);
+		return sha1($this->nonce . json_encode($this->data) . $this->time_stamp . $this->previous_hash);
 	}
 
 	public function import_block($block_json) {
@@ -25,13 +26,14 @@ class Block {
 		$this->hash = $block_json['hash'];
 		$this->previous_hash = $block_json['previous_hash'];
 		$this->time_stamp = $block_json['time_stamp'];
+		$this->nonce = $block_json['nonce'];
 	}
 
 	public function export_block($json = TRUE) {
 		if ($json) {
-			return json_encode(array('time_stamp' => $this->time_stamp, 'data' => $this->data, 'previous_hash' => $this->previous_hash, 'hash' => $this->hash));
+			return json_encode(array('time_stamp' => $this->time_stamp, 'data' => $this->data, 'previous_hash' => $this->previous_hash, 'hash' => $this->hash, 'nonce' => $this->nonce));
 		}
-		return array('time_stamp' => $this->time_stamp, 'data' => $this->data, 'previous_hash' => $this->previous_hash, 'hash' => $this->hash);
+		return array('time_stamp' => $this->time_stamp, 'data' => $this->data, 'previous_hash' => $this->previous_hash, 'hash' => $this->hash, 'nonce' => $this->nonce);
 	}
 
 	public function generate_genesis_block() {
--- index.php
+++ index.php
@@ -9,13 +9,19 @@ $node->route('/index', function($data = []) use ($node){
 	response::render_text('This is a Shelter Chain Node');
 });
 
+$node->route('/dashboard', function($data = []) use ($node){
+	response::render_template('header');
+	response::render_template('dashboard');
+	response::render_template('footer');
+});
+
 $node->route('/chain', function($data = []) use ($node){
 	#Opens up blockchain file.
 	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
 
 	#If the blockchain file is empty, make a new blockchain file.
 	if (sizeof($chain_data) == 0) { 
-		$block = new Block('','');
+		$block = new Block('','','00000000000');
 		$block->generate_genesis_block();
 		array_push($chain_data, $block->export_block($json = false));
 	}
@@ -27,75 +33,55 @@ $node->route('/chain', function($data = []) use ($node){
 });
 
 $node->route('/mine', function($data = []) use ($node){
+	header('Access-Control-Allow-Origin: *');
 	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
 	if (sizeof($chain_data) == 0) {
-		$block = new Block('','');
-		$block->generate_genesis_block();
-		array_push($chain_data, $block->export_block($json = false));
-	}
-
-	if (sizeof($chain_data) != $_POST['chain_size']) {
-		$end_block = array_pop($chain_data);
-		$data_to_edit = $end_block['data'];
-		$previous_hash = $end_block['hash'];
-
-		$data_to_edit[$_POST['shelter_id']] = array($_POST['org_name'], $_POST['street_name'],$_POST['city_name'],$_POST['state_name'],$_POST['zip_code'],$_POST['available'],$_POST['pets'],$_POST['ada'],$_POST['lat'],$_POST['lng']);
-		$new_block = new Block($data_to_edit, $previous_hash);
-		array_push($chain_data, $end_block);
-		array_push($chain_data, $new_block->export_block($json = false));
-		file_put_contents('blockchain.json', json_encode($chain_data));	
-
-		#Send Block Information to all of the Peers in the network.
-		$peers = json_decode(file_get_contents('peers.json'),true);
-		foreach ($peers as $peer) {
-			$data = array(
-				'shelter_id' => $_POST['shelter_id'],
-				'org_name' => $_POST['org_name'],
-				'street_name' => $_POST['street_name'],
-				'city_name' => $_POST['city_name'],
-				'state_name' => $_POST['state_name'],
-				'zip_code' => $_POST['zip_code'],
-				'available' => $_POST['available'],
-				'pets' => $_POST['pets'],
-				'ada' => $_POST['ada'],
-				'lat' => $_POST['lat'],
-				'lng' => $_POST['lng'],
-				'chain_size' => sizeof($chain_data)
-			);
-
-			$url = $peer . "/?r=/mine";
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($ch);
-			curl_close($ch);
-		}	
-	}
-});
-
-$node->route('/test-mine', function($data = []) use ($node){
-	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
-	if (sizeof($chain_data) == 0) {
-		$block = new Block('','');
+		$block = new Block('','','00000000000');
 		$block->generate_genesis_block();
 		array_push($chain_data, $block->export_block($json = false));
 	}
 	$end_block = array_pop($chain_data);
 	$data_to_edit = $end_block['data'];
 	$previous_hash = $end_block['hash'];
-
-	$data_to_edit['nila'] = array(1,2,3,4,5,6,7); 
-	$new_block = new Block($data_to_edit, $previous_hash);
+	$time_stamp = $_POST['time_stamp'] ?: time();
+	$data_to_edit[md5($_POST['org_name'])] = array($_POST['org_name'], $_POST['street_name'],$_POST['city_name'],$_POST['state_name'],$_POST['zip_code'],$_POST['available'],$_POST['pets'],$_POST['ada'],$_POST['lat'],$_POST['lng'],$time_stamp);
+	$new_block = new Block($data_to_edit, $previous_hash, $time_stamp);
 	array_push($chain_data, $end_block);
 	array_push($chain_data, $new_block->export_block($json = false));
 	file_put_contents('blockchain.json', json_encode($chain_data));	
+
+	echo "Block mined";
+
+	$peers = json_decode(file_get_contents('peers.json'),true);
+	foreach ($peers as $peer) {
+		$data = array(
+			'org_name' => $_POST['org_name'],
+			'street_name' => $_POST['street_name'],
+			'city_name' => $_POST['city_name'],
+			'state_name' => $_POST['state_name'],
+			'zip_code' => $_POST['zip_code'],
+			'available' => $_POST['available'],
+			'pets' => $_POST['pets'],
+			'ada' => $_POST['ada'],
+			'lat' => $_POST['lat'],
+			'lng' => $_POST['lng'],
+			'time_stamp' => $time_stamp
+		);
+
+		$url = $peer . "/?r=/mine";
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
+	}
 });
 
 $node->route('/last-block', function($data = []) use ($node){
 	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
 	if (sizeof($chain_data) == 0) {
-		$block = new Block('','');
+		$block = new Block('','','00000000000');
 		$block->generate_genesis_block();
 		array_push($chain_data, $block->export_block($json = false));
 	}
@@ -106,7 +92,7 @@ $node->route('/last-block', function($data = []) use ($node){
 $node->route('/last-block/data', function($data = []) use ($node){
 	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
 	if (sizeof($chain_data) == 0) {
-		$block = new Block('','');
+		$block = new Block('','','00000000000');
 		$block->generate_genesis_block();
 		array_push($chain_data, $block->export_block($json = false));
 	}
@@ -114,19 +100,4 @@ $node->route('/last-block/data', function($data = []) use ($node){
 	response::render_json($end_block['data']);
 });
 
-
-
 $node->start($_GET['r']);
-
-#id
-#name of organization
-#street name
-#city
-#state
-#zip code
-#available
-#pets
-#disability accessible
-#lat
-#lng
-#chain_size
