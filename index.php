0b57daebc9763b1b4eda8443d0dd1a70cca5fb3a
Nile Dixon <niledixon475@gmail.com>
2018-09-14 09:34:18 -0500

    Improved UI

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "block.php";
 
 $node = new Lily;
 $node->route('/index', function($data = []) use ($node){
-	response::render_text('This is a Shelter Chain Node');
+	response::redirect_to('dashboard');
 });
 
 $node->route('/dashboard', function($data = []) use ($node){
@@ -30,6 +30,36 @@ $node->route('/chain', function($data = []) use ($node){
 
 	#Prints out blockchain file.
 	response::render_json($chain_data);
+});
+
+$node->route('/join-peer', function($data = []) use ($node){
+	header('Access-Control-Allow-Origin: *');
+	$peer_data = json_decode(file_get_contents('peers.json'),true);
+	$duplicate_peer = json_decode(file_get_contents('peers.json'),true);
+	$new_peer = $_POST['peer'];
+	if (!in_array($new_peer, $peer_data)) {
+		array_push($peer_data, $new_peer);
+		for ($i=0; $i < sizeof($duplicate_peer); $i++) { 
+			echo $peer;
+			$peer = $duplicate_peer[$i];
+			$data = array(
+				'peer' => $new_peer
+			);
+
+			$url = $peer . "/?r=/join-peer";
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$response = curl_exec($ch);
+			curl_close($ch);
+		}
+		file_put_contents("peers.json", json_encode($peer_data));
+		echo "Peer Added.";
+	}
+	else {
+		echo "Peer was already added to network.";
+	}
 });
 
 $node->route('/mine', function($data = []) use ($node){
@@ -72,7 +102,7 @@ $node->route('/mine', function($data = []) use ($node){
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$response = curl_exec($ch);
 		curl_close($ch);
 	}
@@ -99,5 +129,76 @@ $node->route('/last-block/data', function($data = []) use ($node){
 	$end_block = array_pop($chain_data);
 	response::render_json($end_block['data']);
 });
+
+$node->route('/nearest-shelter', function($data = []) use ($node){
+	header("content-type: text/xml");
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$address = search_for_address($_POST['Body']);
+	if ($address == null) {
+		echo '<Response><Message>Hello, this is a Hurricane Shelter Textbot, helping you find the nearest hurricane shelter. Please type your zip code.</Message></Response>';
+	}
+	else {
+		$coords = get_coords($address);
+		$closest_org = get_closest_org($coords['lat'], $coords['lng']);
+		if ($closest_org == null) {
+			echo "<Response><Message>An error occurred. Please try again.</Message></Response>";
+		}
+		else {
+			echo return_message($closest_org['shelter'],$closest_org['address']);
+		}	
+	}
+});
+
+
+function search_for_address($text) {
+	$address = null;
+	$words = explode(" ", $text);
+	for ($i = 0; $i < sizeof($text); $i++) {
+		$word = preg_replace("/[^a-zA-Z 0-9]+/", "", $words[$i]);
+		if (strlen($word) == '5') {
+			if (is_numeric($word)) {
+				$address = $word;
+				return $address;
+			}
+		}
+	}
+	return $address;
+}
+
+function get_coords($address) {
+	$coords = array();
+	$url = 'https://maps.googleapis.com/maps/api/geocode/json?address='.$address.'&key=AIzaSyD2uPNooAUapFrrB8gRkN3tsPj4kRlgKgw';
+	$data = json_decode(file_get_contents($url),true);
+	$coords['lat'] = $data['results'][0]['geometry']['location']['lat'];
+	$coords['lng'] = $data['results'][0]['geometry']['location']['lng'];
+	return $coords;
+}
+
+function get_closest_org($lat, $lng) {
+	$chain_data = json_decode(file_get_contents('blockchain.json'),true);
+	if (sizeof($chain_data) == 0) {
+		$block = new Block('','','00000000000');
+		$block->generate_genesis_block();
+		array_push($chain_data, $block->export_block($json = false));
+	}
+	$end_block = array_pop($chain_data);
+	if (sizeof($end_block) == 0) {
+		return null;
+	}
+	$nearest_data = null;
+	$shortest_distance = 100000;
+	foreach ($end_block['data'] as $shelter) {
+		if (sqrt(pow(($lat - $shelter[8]), 2) + pow(($lng - $shelter[9]), 2)) < $shortest_distance) {
+			$shortest_distance = sqrt(pow(($lat - $shelter[8]), 2) + pow(($lng - $shelter[9]), 2));
+			$nearest_data = $shelter;
+		}
+	}
+	return $nearest_data;
+}
+
+function return_message ($org_name, $address) {
+	return "<Response><Message>The nearest hurricane shelter is $org_name at $address. For more information, go to florenceresponse.org.</Message></Response>";
+}
+
 
 $node->start($_GET['r']);
--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,3 +1,35 @@
+<div class = "container">
+  <div class = "card">
+    <div class = "container">
+      <div class = "row">
+        <h3 class = "text-xs-center">Shelter Data</h3>
+      </div>
+      <div class = "row">
+        <div class = " container table-responsive">
+          <table class = "table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">City</th>
+                <th scope="col">State</th>
+                <th scope="col">Zip</th>
+                <th scope="col">Pets Allowed</th>
+                <th scope="col">ADA Compliant</th>
+                <th scope="col">Shelter Open?</th>
+                <th scope="col">Latitude</th>
+                <th scope="col">Longitude</th>
+                <th scope="col">Edit</th>
+              </tr>
+            </thead>
+            <tbody id = "shelters-info">
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class = "container" style="margin-top: 50px;">
   <div class = "card">
     <div class = "container" style = "padding: 10px;">
@@ -93,14 +125,27 @@
       </div>
 
       <div class = "row">
-        <button class="btn btn-primary" style = "margin-left: 45%;"  id = "mine_a_block">Submit</button>
+        <div class = "container text-center">
+          <button class = "btn btn-primary" id = "clear_data">Clear Data</button>
+          <button class="btn btn-primary" id = "mine_a_block">Submit</button>
+        </div>
       </div>
     </div>
   </div>
 </div>
+<br>
+<div class = "container">
+  <ul class = "list-group">
+    <li class = "list-group-item">
+      <span style = "float: left;">Add Peer</span>
+      <input style = "float: left;" type = "text" class = "form-control" id = "peer" placeholder="Insert URL of Peer Here"> 
+      <button class = "btn btn-primary" id = "add-peer" style = "float:right;">Add Peer</button>
+    </li>
+  </ul>
+</div>
 <script type="text/javascript">
-  document.getElementById('mine_a_block').addEventListener('click', mine_block);
-  function mine_block() {
+  var data;
+  document.getElementById('mine_a_block').addEventListener('click', function(){
     var xhttp = new XMLHttpRequest();
     var org_name = document.getElementById('org_name').value;
     var street_name = document.getElementById('street_name').value;
@@ -131,5 +176,88 @@
       + "&available=" + available
       + "&lat=" + lat
       + "&lng=" + lng );
+    load_shelter_data();
+  });
+
+  document.getElementById('clear_data').addEventListener('click', function(){
+    document.getElementById('org_name').value = "";
+    document.getElementById('street_name').value = "";
+    document.getElementById('city_name').value = "";
+    document.getElementById('state_name').value = "";
+    document.getElementById('zip_code').value = "";
+    document.getElementById('pets').value = "Yes";
+    document.getElementById('available').value = "Yes" ;
+    document.getElementById('ada').value =  "Yes";
+    document.getElementById('latitude').value = "";
+    document.getElementById('longitude').value = "";
+  });
+
+  document.getElementById('add-peer').addEventListener('click',function(){
+    var xhttp = new XMLHttpRequest();
+    var peer_url = document.getElementById('peer').value;
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        alert(this.responseText);
+      }
+    }
+    xhttp.open("POST", "?r=/join-peer", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(
+      "peer=" + peer_url 
+    );
+  });
+
+  function load_shelter_data() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+        display_shelter_data();
+      }
+    }
+    xhttp.open("POST", "?r=/last-block/data", true);
+    xhttp.send();
   }
+
+  function display_shelter_data() {
+    var shelter_data_display_area = document.getElementById('shelters-info');
+    shelter_data_display_area.innerHTML = "";
+    var string_to_append = "";
+    for (var shelter_info in data) {
+      if (data[shelter_info][6] == 'Yes' ) {
+        string_to_append += '<tr class = "table-success">';
+      }
+      else {
+        string_to_append += '<tr class = "table-danger">';
+      }
+      string_to_append += '<td>' + data[shelter_info][0] + '</td>' +
+      '<td>' + data[shelter_info][1] + '</td>' +
+      '<td>' + data[shelter_info][2] + '</td>' +
+      '<td>' + data[shelter_info][3] + '</td>' +
+      '<td>' + data[shelter_info][4] + '</td>' +
+      '<td>' + data[shelter_info][5] + '</td>' +
+      '<td>' + data[shelter_info][6] + '</td>' +
+      '<td>' + data[shelter_info][7] + '</td>' +
+      '<td>' + data[shelter_info][8] + '</td>' +
+      '<td>' + data[shelter_info][9] + '</td>' +
+      '<td><button class = "btn btn-primary" onclick=\'update_data("' +shelter_info+ '")\'>Edit</button></td>' +
+      '<tr>';
+    }
+    shelter_data_display_area.innerHTML = string_to_append;
+  }
+
+  function update_data(shelter_id) {
+    document.getElementById('org_name').value = data[shelter_id][0];
+    document.getElementById('street_name').value = data[shelter_id][1];
+    document.getElementById('city_name').value = data[shelter_id][2];
+    document.getElementById('state_name').value = data[shelter_id][3];
+    document.getElementById('zip_code').value = data[shelter_id][4];
+    document.getElementById('pets').value = data[shelter_id][5];
+    document.getElementById('available').value = data[shelter_id][6];
+    document.getElementById('ada').value = data[shelter_id][7];
+    document.getElementById('latitude').value = data[shelter_id][8];
+    document.getElementById('longitude').value = data[shelter_id][9];
+  }
+
+  load_shelter_data();
 </script>
